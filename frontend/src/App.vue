ea50d28e10e1d8c3283ed3cb6a1fa2d1d00bb9ef
Karubin <svrcina.mar@gmail.com>
2026-08-06 20:28:48 +0200

    Scaffold Vue 3 + Vuetify + Pinia frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar title="Správa skladu" />
+    <v-main>
+      <v-container>
+        <p>Aplikace běží.</p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+import '@mdi/font/css/materialdesignicons.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import { vuetify } from './plugins/vuetify'
+
+createApp(App)
+  .use(createPinia())
+  .use(vuetify)
+  .mount('#app')
